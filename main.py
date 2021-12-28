38785257079c2212a185d66b4538131e14d15b45
Danilo Xaxa <daniloxaxa01@gmail.com>
2021-12-28 13:41:38 -0300

    criando db

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+from typing import List
+from uuid import uuid4
 from fastapi import FastAPI
+from models import Gender, User, Role
 
 
 app = FastAPI()
+
+db: List[User] = [
+    User(
+        id=uuid4(), 
+        first_name="Jamila",
+        last_name="Ahmed",
+        gender=Gender.female,
+        roles=[Role.student]
+    ),
+    User(
+        id=uuid4(), 
+        first_name="Alex",
+        last_name="Jones",
+        gender=Gender.male,
+        roles=[Role.admin, Role.user] 
+    ),
+]
 
 @app.get("/")
 async def root():
