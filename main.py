bff30c52e9fa0ec5636cf56cf36e83f360d423fe
Danilo Xaxa <daniloxaxa01@gmail.com>
2021-12-29 20:22:13 -0300

    put básico, mas funcionando já

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, HTTPException, Request
 from utilidades.models import Contato, ContatoUpdate, db
 import hubspot
 from hubspot.crm.contacts import ApiException, SimplePublicObjectInput
+import requests
 
 
 app = FastAPI()
@@ -17,7 +18,7 @@ async def index():
 
 
 @app.get("/crm/v3/objects/contacts/{api_key}")
-async def ler_contato(api_key: str = API_KEY, contact: str = None, properties: str = None):
+async def ler_contatos(api_key: str = API_KEY, contact: str = None, properties: str = None):
     client = hubspot.Client.create(api_key=api_key)
 
     try:
@@ -61,7 +62,7 @@ async def criar_contato(api_key: str = API_KEY, request: Request = None):
     except Exception as e:
         return HTTPException(status_code=400, detail="Erro no JSON")
 
-    todos_contatos = await ler_contato(api_key=api_key)
+    todos_contatos = await ler_contatos(api_key=api_key)
     if [contato for contato in todos_contatos if contato['email'] == properties['email']]:
         # nesse caso, atualizar o contato
         return HTTPException(status_code=400, detail="Contato já existe")
@@ -75,7 +76,32 @@ async def criar_contato(api_key: str = API_KEY, request: Request = None):
         return HTTPException(status_code=500, detail="Erro ao criar contato")
 
 
-@app.put("/crm/v3/objects/contacts/{api_key}")
+@app.put("/crm/v3/objects/contacts/{api_key}/{contact}")
 async def atualizar_contato(api_key: str = API_KEY, contact: str = None, request: Request = None):
+    if not contact:
+        return HTTPException(status_code=400, detail="Contato não informado")
+
     # caso o contact (email) não exista, cria com o post acima
-    ...
+
+    client = hubspot.Client.create(api_key=api_key)
+
+    try:
+        properties = await request.json()
+    except Exception as e:
+        return HTTPException(status_code=400, detail="Erro no JSON")
+
+    todos_contatos = await ler_contatos(api_key=api_key)
+
+    if not [contato for contato in todos_contatos if contato['email'] == contact]:
+        return HTTPException(status_code=400, detail="Contato não existe")
+
+    resposta = requests.get(f"https://api.hubapi.com/contacts/v1/contact/emails/batch/?email={contact}&hapikey={api_key}").json()
+    contact_id = [*resposta][0]
+
+    try:
+        simple_public_object_input = SimplePublicObjectInput(properties=properties)
+        api_response = client.crm.contacts.basic_api.update(contact_id=contact_id, simple_public_object_input=simple_public_object_input)
+        return properties
+    except ApiException as e:
+        print("Exception when calling basic_api->update: %s\n" % e)
+        return HTTPException(status_code=500, detail="Erro ao atualizar contato")
