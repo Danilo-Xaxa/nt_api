4273af7f369e5b1b5a67877ae4962b9ea38495e0
Danilo Xaxa <daniloxaxa01@gmail.com>
2021-12-31 16:40:43 -0300

    testando um action básico

--- front/script.js
+++ front/script.js
@@ -1,11 +1,16 @@
-function formDataToJSON(form) {
-    let formData = new FormData(document.getElementById(form));
+function formDataToJSON() {
+    let form = document.getElementById('formulario');
+    let formData = new FormData(form);
     let obj = {};
     formData.forEach(function(value, key){
       obj[key] = value;
     });
-    let json = JSON.stringify(obj);
 
-    console.log(json);
+    // let json = JSON.stringify(obj);
+    // console.log(json);
+
+    //form.action = 'http://localhost:8000/crm/v3/objects/contacts/' + obj.apiKey;
+    //return true;
+
     return false; // false to cancel form action, true to submit form
 }
