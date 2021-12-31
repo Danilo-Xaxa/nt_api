f8afa171c537071cb05178d28b48a9002200b858
Danilo Xaxa <daniloxaxa01@gmail.com>
2021-12-31 19:31:16 -0300

    ver funcionando perfeitamente

--- front/script.js
+++ front/script.js
@@ -1,8 +1,5 @@
-const form = document.getElementsByTagName('form')[0];
-
-
 function formDataToJSON() {
-  let formData = new FormData(form);
+  let formData = new FormData(document.getElementsByTagName('form')[0]);
   let obj = {};
   formData.forEach(function(value, key){
     obj[key] = value;
@@ -12,20 +9,23 @@ function formDataToJSON() {
 }
 
 
-function setAction(tipo, json) {
+function setAction(tipo, lista) {
+    const primeiroForm = document.getElementsByTagName('form')[0];
     switch (tipo) {
         case 'ver':
-            form.action = 'localhost:8000/crm/v3/objects/contacts/' + json.apiKey;
+            let queryParameters = lista[0] ? '?' + lista[0] : '';
+            let finalURL = 'localhost:8000/crm/v3/objects/contacts/' + lista[1] + queryParameters
+            return finalURL;
             break;
 
         case 'criar':
-            form.action = 'localhost:8000/crm/v3/objects/contacts/' + json.apiKey;
-            // mas e o body?
+            primeiroForm.action = 'localhost:8000/crm/v3/objects/contacts/' + lista[1];
+            // body = lista[0];
             break;
 
         case 'atualizar':
-            form.action = 'localhost:8000/crm/v3/objects/contacts/' + json.apiKey + json.contact;
-            // mas e o body?
+            primeiroForm.action = 'localhost:8000/crm/v3/objects/contacts/' + lista[1] + lista[2];
+            // body = lista[0];
             break;
 
         default:
@@ -34,15 +34,55 @@ function setAction(tipo, json) {
 }	
 
 
-function formatJSON(json) {
-    // só falta você, meu querido
+function formatJSON(tipo, json) {
+    switch (tipo) {
+        case 'ver':
+            let properties = ''
+            for (let property in json) {
+                if (['email', 'telefone', 'niver', 'peso'].includes(property.toString())) {
+                    properties = properties + `${property}-`
+                }
+            }
+            if (properties.endsWith('-')) {
+                properties = properties.slice(0, -1);
+            }
+            if (properties) {
+                properties = 'properties=' + properties;
+            }
+            if (json.contact) {
+                let contact = 'contact=' + json.contact;
+                let queryParameters = properties ? contact + '&' + properties : contact;
+                return [queryParameters, json.apiKey];
+            }
+            let queryParameters = properties;
+            return [queryParameters, json.apiKey];
+            break;
+
+        case 'criar':
+            return [json, json.apiKey]; // retorna um objeto para o body
+            break;
+
+        case 'atualizar':
+            let jsonNovo = {}
+            for (let property in json) {
+                if (property in ['email', 'telefone', 'niver', 'peso']) {
+                    jsonNovo[property] = json[property];
+                }
+            }
+            return [json, json.apiKey, json.contact]
+            break;
+
+        default:
+            throw new Error('ERRO');
+            break;
+    }
 }
 
 
-function handle() {
+function linkFinal() {
+    let primeiroForm = document.getElementsByTagName('form')[0];
+    let tipo = primeiroForm.id.split('_')[1];
     let json = formDataToJSON();
-    json = formatJSON(json);
-    let tipo = form.id.split('_')[1]
-    setAction(tipo, json)
-    return true; // false to cancel form action, true to submit form
+    lista = formatJSON(tipo, json);
+    return 'http://' + setAction(tipo, lista);
 }
