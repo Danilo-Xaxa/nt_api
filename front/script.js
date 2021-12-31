a89bfde76f1d25042f48e1e0883a8bf7a67c16ae
Danilo Xaxa <daniloxaxa01@gmail.com>
2021-12-31 16:43:10 -0300

    Revert "testando um action básico"
    This reverts commit 4273af7f369e5b1b5a67877ae4962b9ea38495e0.

--- front/script.js
+++ front/script.js
@@ -1,16 +1,11 @@
-function formDataToJSON() {
-    let form = document.getElementById('formulario');
-    let formData = new FormData(form);
+function formDataToJSON(form) {
+    let formData = new FormData(document.getElementById(form));
     let obj = {};
     formData.forEach(function(value, key){
       obj[key] = value;
     });
+    let json = JSON.stringify(obj);
 
-    // let json = JSON.stringify(obj);
-    // console.log(json);
-
-    //form.action = 'http://localhost:8000/crm/v3/objects/contacts/' + obj.apiKey;
-    //return true;
-
+    console.log(json);
     return false; // false to cancel form action, true to submit form
 }
