1cc06be2948b1d717b85d9a4a728a7e1ec14182e
Danilo Xaxa <daniloxaxa01@gmail.com>
2022-01-02 20:48:15 -0300

    post funcionando perfeitamente

--- front/criar.js
+++ front/criar.js
@@ -23,7 +23,21 @@ function criar() {
     let valores = formatJSON(formDataToJSON(formulario));
     let urlFinal = "http://127.0.0.1:8000/crm/v3/objects/contacts/" + valores.apiKey;
     let jsonBody = valores.jsonBody;
-    jsonBody = JSON.stringify(jsonBody);
 
-    return false;
+    (async () => {
+        const rawResponse = await fetch(urlFinal, {
+          method: 'POST',
+          mode: 'cors',
+          headers: {
+            'Accept': '*',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(jsonBody)
+        });
+        const content = await rawResponse.json();
+      
+        console.log(content);
+      })();
+
+    return false; // ou true? ou nada?
 };
